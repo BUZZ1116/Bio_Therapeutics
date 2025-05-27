6ae394df44e56bbdf41833353f4590cb4772b2af
BUZZ1116 <110268621+BUZZ1116@users.noreply.github.com>
2025-05-27 15:56:49 +0800

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const footerHTML = `
+    <footer>
+      <p>&copy; 2025 綠上商城. All rights reserved.</p>
+    </footer>`;
+  document.getElementById("footer").innerHTML = footerHTML;
+});
