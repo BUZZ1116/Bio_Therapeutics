c6fe2a3dacfd589cbc45903686be756a2ba67f42
BUZZ1116 <110268621+BUZZ1116@users.noreply.github.com>
2026-08-25 12:41:51 +0800

    Delete footer.js

--- footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const footerHTML = `
-    <footer>
-      <p>&copy; 2025 綠上商城. All rights reserved.</p>
-    </footer>`;
-  document.getElementById("footer").innerHTML = footerHTML;
-});
